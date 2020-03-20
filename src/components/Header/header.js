87fdd86575906e6d11d0588c82026fb376ab7f22
sullom101 <mayow.sullom@gmail.com>
2020-03-21 03:48:25 +0800

    update header and styled header component

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,42 +1,32 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+} from "reactstrap"
+import Head from "../StyledComponents/header"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = props => {
+  const [collapsed, setCollapsed] = useState(true)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+  const toggleNavbar = () => setCollapsed(!collapsed)
+
+  return (
+    <Head>
+      <Container className="container" fluid={true}>
+        <Navbar color="faded" light>
+          <NavbarBrand href="/" className="mr-auto">
+            {props.siteTitle}
+          </NavbarBrand>
+        </Navbar>
+      </Container>
+    </Head>
+  )
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header;
+export default Header
---
+++ src/components/StyledComponents/header.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Header = styled.header`
+    padding: 1rem 0;
+    border-bottom:1px solid black;
+`;
+
+export default Header;
