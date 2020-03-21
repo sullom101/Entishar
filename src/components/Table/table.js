90d40fdee81749f0d100470fa03028cc72e1e420
sullom101 <mayow.sullom@gmail.com>
2020-03-22 03:08:57 +0800

    fetchData && Translate countries to arabic

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -1,18 +1,35 @@
-import React from "react"
-import axios from 'axios';
-import { Table,  Label, Input } from "reactstrap"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import { Table, Label, Input } from "reactstrap"
+import { countryTransformer } from "../../utils/countryTransformer"
 
-const url = 'https://api.covid19api.com/summary';
-
+const url = "https://api.covid19api.com/summary"
 
 const TableData = () => {
-    const fetch = fetchData();
-    
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios
+        .get(url)
+        .then(res => {
+          console.log("outside the fucking function", res.data.Countries)
+          return res.data.Countries
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      setData(result)
+    }
+
+    fetchData()
+  }, [])
+
+  
 
   return (
     <div className="card" style={{ marginTop: "1rem", marginBottom: "1rem" }}>
-
-      <div className>
+      <div>
         <Label
           for="searchQuery"
           size="lg"
@@ -24,52 +41,49 @@ const TableData = () => {
           <Input type="text" name="search" />
         </Label>
       </div>
-    
-    
-      <Table bordered>
+      <Table bordered dir="rtl" style={{marginBottom:0}}>
         <thead>
           <tr>
-            <th>TOTAL (158)</th>
-            {/* <th>CONFIRMED</th>
-            <th>DEATH</th>
-            <th>Recovered</th>
-            <th>FATALITY RATE</th>
-            <th>RECOVERY RATE</th> */}
+            <th>مجموع الدول المصابة {data !== null ? "( " + data.length + " )" : "(...)"}</th>
+            <td> الحالات الجديدة </td>
+                  <td> جميع الحالات المصابة</td>
+                  <td>حالات الوفيات الجديدة </td>
+                  <td> جميع حالات الوفيات </td>
+                  <td> حالات التعافي الجديدة </td>
+                  <td> جميع حالات التعافي </td>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            {/* <th scope="row">Country</th> */}
-            {/* <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>Mark</td>
-            <td>Otto</td> */}
-          </tr>
+          {/* <tr> */}
+          {data == null
+            ? "loading...."
+            : data.map((el, index) => (
+                <tr key={index}>
+                  <td> {countryTransformer(el.Country)} </td>
+                  <td> {el.NewConfirmed} </td>
+                  <td> {el.TotalConfirmed} </td>
+                  <td> {el.NewDeaths} </td>
+                  <td> {el.TotalDeaths} </td>
+                  <td> {el.NewRecovered} </td>
+                  <td> {el.TotalRecovered} </td>
+                </tr>
+              ))}
+          
         </tbody>
         <tfoot>
-          <tr>
-            <th>TOTAL </th>
-            {/* <th>All CONFIRMED Cases </th>
-            <th>All DEATH Cases</th>
-            <th>All Recovered cases</th>
-            <th>FATALITY RATE</th>
-            <th>RECOVERY RATE</th> */}
+          <tr >
+            <th>الاجمالي </th>
+            <th> اجمالي الاصابات الحديثة  </th>
+            <th>اجمالي الحالات </th>
+            <th>اجمالي الوفيات الحديثة</th>
+            <th>اجمالي الوفيات </th>
+            <th>اجمالي  التعافي الحديثة </th>
+            <th>اجمالي التعافي</th>
           </tr>
         </tfoot>
       </Table>
     </div>
   )
 }
-
-
-const fetchData = async ()=>{
-    
-    const fetch = await axios.get(url).then( res=> res.data );
-    console.log('outside the fucking function', fetch.Countries);
-    return fetch;
-} 
-
-
 
 export default TableData
