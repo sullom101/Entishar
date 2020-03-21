6f772eff0f2ebeaafb46a7d90abb1622e4641657
sullom101 <mayow.sullom@gmail.com>
2020-03-22 02:57:04 +0800

    remove unused imports

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,20 +1,12 @@
-import React, { useState } from "react"
+import React from "react"
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
   Container,
 } from "reactstrap"
 import Head from "../StyledComponents/header"
 
 const Header = props => {
-  const [collapsed, setCollapsed] = useState(true)
-
-  const toggleNavbar = () => setCollapsed(!collapsed)
 
   return (
     <Head>
