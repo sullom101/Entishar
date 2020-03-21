7e21c296bc717575d45a2fbda4f773e3ed586d89
sullom101 <mayow.sullom@gmail.com>
2020-03-22 02:56:29 +0800

    fix iframe eslint warnings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/Layout/layout"
-import Image from "../components/image"
 import SEO from "../components/SEO/seo"
 import Table from "../components/Table/table"
 import MainStat from "../components/MainStat/mainStat"
@@ -21,10 +18,10 @@ const IndexPage = () => (
         width="100%"
         height="550"
         src="https://www.outbreak.my/widget/viruscompare"
-        frameborder="0"
+        frameBorder="0"
         allow="autoplay; encrypted-media;
 picture-in-picture"
-        allowfullscreen
+        allowFullScreen={true}
       ></iframe>
     
     
