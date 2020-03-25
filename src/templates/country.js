95f9055b7cc1fcd3c2d0bd467931a80a4a44fc9b
sullom101 <mayow.sullom@gmail.com>
2020-03-25 22:07:34 +0800

    update country template

--- src/templates/country.js
+++ src/templates/country.js
@@ -1,30 +1,28 @@
-import React from "react"
-import Layout from "../components/Layout/layout"
+import React, { useState, useEffect } from "react"
 import { graphql } from "gatsby"
 
+import CountryPage from "../pages/country"
+
 export default ({ data }) => {
-  const post = data.allInternalCountries.edges[0].node
-  console.log(post)
-  return (
-    <Layout>
-      <div>
-        <h1>{post.Country} Country Stats</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.Slug }} />
-      </div>
-    </Layout>
-  )
+  const countryData = data.allInternalCountries.edges[0].node
+
+
+  console.log('data is',countryData)
+  
+    return <CountryPage  data={countryData} />
+  
+  
 }
 export const query = graphql`
   query($slug: String!) {
-      
-      allInternalCountries (filter: { Slug: { eq: $slug } }) {
-        edges {
-          node {
-            Country
-            Provinces
-            Slug
-          }
+    allInternalCountries(filter: { Slug: { eq: $slug } }) {
+      edges {
+        node {
+          Country
+          Provinces
+          Slug
         }
       }
+    }
   }
 `
