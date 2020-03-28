690e635a42ed3f54382386ed80e52627722e3e9d
sullom101 <mayow.sullom@gmail.com>
2020-03-29 02:44:33 +0800

    get deaths by country data

--- src/components/CountryInfo/Summary.js
+++ src/components/CountryInfo/Summary.js
@@ -5,11 +5,13 @@ import CountryInfoCard from "../StyledComponents/CountryInfoCard"
 import MainCountryStatWrapper from "../StyledComponents/MainCountryStatWrapper"
 import DailyGraphWrapper from "../StyledComponents/dailyGraphWrapper"
 import Visualiz from "./visualiz"
-
+import CasesbyState from './CasesByStates'
 const Summary = props => {
   const [summary, setSummary] = useState(null)
   const [countryConfirmed, setCountryConfirmed] = useState(null)
-
+  const [label, setLabel]=useState(null)
+  const [dataset, setdataset]=useState(null)
+  const [deaths, setDeaths]=useState(null);
 
   useEffect(() => {
     const summaryData = async () => {
@@ -25,26 +27,61 @@ const Summary = props => {
     }
     const countryConfirmed = async () => {
       const getConfirmed = await axios
-        .get(`https://api.covid19api.com/country/${props.data.Slug}/status/confirmed`)
+        .get(`https://api.covid19api.com/total/country/${props.data.Slug}/status/confirmed`)
         .then(res => {
           console.log('this is a test for confirmed cases',res.data)
           return res.data;
         })
         .catch(err => console.log(err))
+
+      let templabel = []
+      let tempData =[]
+      getConfirmed.map(el=>{
+        templabel.push(dateAxis(el.Date));
+        tempData.push(el.Cases)
+      })
+      setLabel(templabel)
+      setdataset(tempData);
+      console.log("loging temp data data", tempData)
+      console.log("loging temp date as a label", templabel)
       console.log("loging returned confirmed data", getConfirmed)
       setCountryConfirmed(getConfirmed)
     }
 
+    const countryDeaths = async () => {
+      const getDeaths = await axios
+        .get(`https://api.covid19api.com/total/country/${props.data.Slug}/status/deaths`)
+        .then(res => {
+          console.log('this is a test for confirmed cases',res.data)
+          return res.data;
+        })
+        .catch(err => console.log(err))
+      let tempData =[]
+      getDeaths.map(el=>{
+        tempData.push(el.Cases)
+      })
+      
+      setDeaths(tempData)
+    }
+
     summaryData()
     countryConfirmed()
+    countryDeaths()
     console.log("getSummary Data", summary)
   }, [])
+
+  const dateAxis = date => {
+    var d = new Date(date)
+    let result = d.getDate() + "-0" + parseInt(d.getMonth() + 1)
+    console.log(result)
+    return result
+  }
 
   return (
     <div>
       <MainCountryStatWrapper>
         <CountryInfoCard>
-          <p style={{ fontFamily: "nunito-sans" }}>Total Confirmed</p>
+          <p style={{ fontFamily: "Lato" }}>Total Confirmed</p>
           <p>{summary !== null ? summary.TotalConfirmed : ""}</p>
           <p>{/* {props.TotalConfirmed} */}</p>
         </CountryInfoCard>
@@ -60,10 +97,17 @@ const Summary = props => {
         </CountryInfoCard>
       </MainCountryStatWrapper>
       <DailyGraphWrapper>
-        <CountryInfoCard>
-          {countryConfirmed && summary !== null ?<Visualiz data={countryConfirmed} summary={summary.TotalConfirmed}/>: 'Loading.....'}
+        <CountryInfoCard >
+          {countryConfirmed && summary && label && dataset && deaths !== null ?<Visualiz deathData={deaths} label={label} data={dataset} summary={summary.TotalConfirmed}/>: 'Loading.....'}
         </CountryInfoCard>
       </DailyGraphWrapper>
+      <DailyGraphWrapper>
+        <CountryInfoCard>
+        {countryConfirmed && summary !== null ?<CasesbyState data={countryConfirmed} summary={summary.TotalConfirmed}/>: 'Loading.....'}
+        </CountryInfoCard>
+        
+      </DailyGraphWrapper>
+
     </div>
   )
 }
