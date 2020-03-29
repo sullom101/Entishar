a75439ea72a732254b336799b3475d9e3ee838b2
sullom101 <mayow.sullom@gmail.com>
2020-03-30 06:47:53 +0800

    update summary component part-1

--- src/components/CountryInfo/Summary.js
+++ src/components/CountryInfo/Summary.js
@@ -5,13 +5,14 @@ import CountryInfoCard from "../StyledComponents/CountryInfoCard"
 import MainCountryStatWrapper from "../StyledComponents/MainCountryStatWrapper"
 import DailyGraphWrapper from "../StyledComponents/dailyGraphWrapper"
 import Visualiz from "./visualiz"
-import CasesbyState from './CasesByStates'
+import CasesbyState from "./CasesByStates"
 const Summary = props => {
   const [summary, setSummary] = useState(null)
   const [countryConfirmed, setCountryConfirmed] = useState(null)
-  const [label, setLabel]=useState(null)
-  const [dataset, setdataset]=useState(null)
-  const [deaths, setDeaths]=useState(null);
+  const [label, setLabel] = useState(null)
+  const [dataset, setdataset] = useState(null)
+  const [deaths, setDeaths] = useState(null)
+  const [recovered, setRecovered] = useState(null)
 
   useEffect(() => {
     const summaryData = async () => {
@@ -27,21 +28,30 @@ const Summary = props => {
     }
     const countryConfirmed = async () => {
       const getConfirmed = await axios
-        .get(`https://api.covid19api.com/total/country/${props.data.Slug}/status/confirmed`)
+        .get(
+          `https://api.covid19api.com/total/country/${props.data.Slug}/status/confirmed`
+        )
         .then(res => {
-          console.log('this is a test for confirmed cases',res.data)
-          return res.data;
+          console.log("this is a test for confirmed cases", res.data)
+          return res.data
         })
         .catch(err => console.log(err))
 
+      const dateAxis = date => {
+        var d = new Date(date)
+        let result = d.getDate() + "-0" + parseInt(d.getMonth() + 1)
+
+        return result
+      }
+      
       let templabel = []
-      let tempData =[]
-      getConfirmed.map(el=>{
-        templabel.push(dateAxis(el.Date));
+      let tempData = []
+      getConfirmed.map(el => {
+        templabel.push(dateAxis(el.Date))
         tempData.push(el.Cases)
       })
       setLabel(templabel)
-      setdataset(tempData);
+      setdataset(tempData)
       console.log("loging temp data data", tempData)
       console.log("loging temp date as a label", templabel)
       console.log("loging returned confirmed data", getConfirmed)
@@ -50,67 +60,79 @@ const Summary = props => {
 
     const countryDeaths = async () => {
       const getDeaths = await axios
-        .get(`https://api.covid19api.com/total/country/${props.data.Slug}/status/deaths`)
+        .get(
+          `https://api.covid19api.com/total/country/${props.data.Slug}/status/deaths`
+        )
         .then(res => {
-          console.log('this is a test for confirmed cases',res.data)
-          return res.data;
+          console.log("this is a test for death cases", res.data)
+          return res.data
         })
         .catch(err => console.log(err))
-      let tempData =[]
-      getDeaths.map(el=>{
+      let tempDeath = []
+
+      getDeaths.map(el => {
+        tempDeath.push(el.Cases)
+      })
+
+      setDeaths(tempDeath)
+    }
+    const countryRecovered = async () => {
+      const getRecovered = await axios
+        .get(
+          `https://api.covid19api.com/total/country/${props.data.Slug}/status/recovered`
+        )
+        .then(res => {
+          console.log("this is a test for Recovered cases", res.data)
+          return res.data
+        })
+        .catch(err => console.log(err))
+
+      let tempData = []
+
+      getRecovered.map(el => {
         tempData.push(el.Cases)
       })
-      
-      setDeaths(tempData)
+
+      setRecovered(tempData)
     }
 
     summaryData()
     countryConfirmed()
     countryDeaths()
+    countryRecovered()
     console.log("getSummary Data", summary)
   }, [])
 
-  const dateAxis = date => {
-    var d = new Date(date)
-    let result = d.getDate() + "-0" + parseInt(d.getMonth() + 1)
-    console.log(result)
-    return result
-  }
-
   return (
     <div>
-      <MainCountryStatWrapper>
-        <CountryInfoCard>
-          <p style={{ fontFamily: "Lato" }}>Total Confirmed</p>
-          <p>{summary !== null ? summary.TotalConfirmed : ""}</p>
-          <p>{/* {props.TotalConfirmed} */}</p>
-        </CountryInfoCard>
-        <CountryInfoCard>
-          <p> Total Recovered</p>
-          <p>{summary !== null ? summary.TotalDeaths : ""}</p>
-        </CountryInfoCard>
-        <CountryInfoCard>
-          <p>Total Deaths </p>
-          <p>
-            {summary !== null ? summary.TotalRecovered : ""} <br />{" "}
-          </p>
-        </CountryInfoCard>
-      </MainCountryStatWrapper>
       <DailyGraphWrapper>
-        <CountryInfoCard >
-          {countryConfirmed && summary && label && dataset && deaths !== null ?<Visualiz deathData={deaths} label={label} data={dataset} summary={summary.TotalConfirmed}/>: 'Loading.....'}
+        <CountryInfoCard>
+          {label && dataset && deaths && recovered !== null ? (
+            <Visualiz
+              deathData={deaths}
+              label={label}
+              recoveredData={recovered}
+              data={dataset}
+            />
+          ) : (
+            "Loading....."
+          )}
         </CountryInfoCard>
       </DailyGraphWrapper>
       <DailyGraphWrapper>
         <CountryInfoCard>
-        {countryConfirmed && summary !== null ?<CasesbyState data={countryConfirmed} summary={summary.TotalConfirmed}/>: 'Loading.....'}
+          {countryConfirmed && summary !== null ? (
+            <CasesbyState
+              data={countryConfirmed}
+              summary={summary.TotalConfirmed}
+            />
+          ) : (
+            "Loading....."
+          )}
         </CountryInfoCard>
-        
       </DailyGraphWrapper>
-
     </div>
   )
 }
-
 
 export default Summary
