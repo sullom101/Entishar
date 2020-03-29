530a9687a7b42b659e9d1153bea2b76a89f507ef
sullom101 <mayow.sullom@gmail.com>
2020-03-30 06:42:15 +0800

    update pages with data

--- src/pages/country-page.js
+++ src/pages/country-page.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
+import axios from 'axios'
 import MainEach from "../components/CountryInfo/MainEach"
 import Layout from "../components/Layout/layout"
 // import SEO from "../components/SEO/seo"
@@ -8,13 +9,33 @@ import Layout from "../components/Layout/layout"
 import Summary from "../components/CountryInfo/Summary"
 
 const SecondPage = props => {
-  console.log()
-  return (
-    <Layout>
-      <Summary data={props.data} />
-      <MainEach data={props.data} />
-    </Layout>
-  )
+  const [summary, setSummary] = useState(null)
+
+  useEffect(() => {
+    const summaryData = async () => {
+      const getSummary = await axios
+        .get(`https://api.covid19api.com/summary`)
+        .then(res => {
+          const find = res.data.Countries.find(el => el.Slug == props.data.Slug)
+          return find
+        })
+        .catch(err => console.log(err))
+      console.log("Country-page data", getSummary)
+      setSummary(getSummary)
+    }
+    summaryData()
+  }, [])
+
+  if (summary !== null) {
+    return (
+      <Layout>
+        <MainEach summary={summary} data={props.data} />
+        <Summary summary={summary} data={props.data} />
+      </Layout>
+    )
+  } else {
+    return ""
+  }
 }
 
 export default SecondPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,78 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../components/Layout/layout"
 import SEO from "../components/SEO/seo"
 import Table from "../components/CountriesTable/table"
 import MainStat from "../components/MainStat/mainStat"
+import axios from "axios"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div>
-      <h1> تحديث مباشر لتفشي فيروس كورونا </h1>
-    </div>
-    <MainStat />
-    <Table />
+const IndexPage = () => {
+  const [summary, setSummary] = useState(null)
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    async function fetchData() {
+      const url = "https://api.covid19api.com/summary"
+      const result = await axios
+        .get(url)
+        .then(res => {
+          console.debug("List of countries ", typeof res.data)
+          if (typeof res.data !== "object") {
+            const parsedData = JSON.parse(res.data)
+            console.log("parsed data", parsedData)
+            return parsedData.Countries
+          } else {
+            return res.data.Countries
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+        const sortedData = result.sort((a,b)=> b.TotalConfirmed - a.TotalConfirmed )
+      setData(sortedData)
 
-    <iframe
-      style={{ width: "100%" }}
-      width="100%"
-      height="550"
-      src="https://www.outbreak.my/widget/viruscompare"
-      frameBorder="0"
-      allow="autoplay; encrypted-media;picture-in-picture"
-      allowFullScreen={true}
-    ></iframe>
-  </Layout>
-)
+      summaryCases(sortedData)
+    }
+    fetchData()
+  }, [])
+
+  const summaryCases = result => {
+    let obj = {}
+    const confirmed = result
+      .map(el => el.TotalConfirmed)
+      .reduce((acc, cur) => acc + cur)
+    const deaths = result
+      .map(el => el.TotalDeaths)
+      .reduce((acc, cur) => acc + cur)
+    const recovered = result
+      .map(el => el.TotalRecovered)
+      .reduce((acc, cur) => acc + cur)
+    obj = {
+      TotalConfirmed: confirmed,
+      TotalDeaths: deaths,
+      TotalRecovered: recovered,
+    }
+    console.log("this is an array of importat data", obj)
+    // return array;
+
+    setSummary(obj)
+  }
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <MainStat summary={summary}/>
+      <Table summary={summary} data={data}/>
+
+      <iframe
+        style={{ width: "100%" }}
+        width="100%"
+        height="550"
+        src="https://www.outbreak.my/widget/viruscompare"
+        frameBorder="0"
+        allow="autoplay; encrypted-media;picture-in-picture"
+        allowFullScreen={true}
+      ></iframe>
+    </Layout>
+  )
+}
 
 export default IndexPage
