bfbbf28f26f4d1cb0155a6213d6fae94d6a5c0e3
sullom101 <mayow.sullom@gmail.com>
2020-03-31 05:11:21 +0800

    prepare cases by day instead of state

---
+++ src/components/CountryInfo/CasesByDay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CasesByDay() {
+    return (
+        <div className="card">
+            <p> Confirmed Cases by Day </p>
+            <p> this is Date </p>
+            <p> The number of confirmed cases are 12</p>
+        </div>
+    )
+}
--- src/components/CountryInfo/CasesByStates.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function CasesByStates() {
-    return (
-        <div>
-            <p> Cases by state</p>
-        </div>
-    )
-}
