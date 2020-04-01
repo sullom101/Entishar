1c471170518bddfbb4297e4bebdad898d775fb24
sullom101 <mayow.sullom@gmail.com>
2020-04-02 03:22:07 +0800

    update footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -6,13 +6,11 @@ const  Footer = ()=> {
   return (
     <Foot>
       <Container>
-        <Row xl="2">
+        <Row>
           <Col className="float-right">
             Copyrights © {new Date().getFullYear()}{" "}
             <a href="https://www.alieniz.com">Alieniz</a>. All rights-reserved
             {` `}
-          </Col>
-          <Col className="float-left">
           </Col>
         </Row>
       </Container>
